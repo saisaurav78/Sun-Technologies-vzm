f3c5ff5833cc8ea2711e60c0ee0681b4cd84b466
saisaurav <saisaurav78@gmail.com>
2024-12-19 17:18:32 +0530

    Added subsets and preload

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import { Roboto } from 'next/font/google'
-const roboto = Roboto({ weight: ['100', '300', '400', '500', '700'] });
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700'],
+  subsets:["latin"],
+  preload:true
+}
+);
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
